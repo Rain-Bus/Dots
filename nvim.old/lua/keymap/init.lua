0f447a2d45b0b22187c564b9e9aeea6e30af5b5d
rain-bus <rainandbus@gmail.com>
2025-12-26 23:21:36 +0800

    Nvim: Remove old

--- nvim.old/lua/keymap/init.lua
+++
@@ -1,6 +0,0 @@
--- author: glepnr https://github.com/glepnir
--- date: 2022-07-02
--- License: MIT
--- recommend plugins key defines in this file
-
-require('keymap.config')
--- nvim.old/lua/modules/editor/plugins.lua
+++
@@ -1,8 +0,0 @@
-local plugin = require('core.pack').register_plugin
-local conf = require('modules.editor.config')
-
-plugin({ "windwp/nvim-autopairs", config = conf.autopair })
-plugin({ "windwp/nvim-ts-autotag", config = conf.autotag })
-plugin({ "Pocco81/auto-save.nvim", config = conf.autosave })
-plugin({ "numToStr/Comment.nvim", config = conf.comment })
--- plugin({ "folke/neoconf.nvim", config = conf.neoconf })
